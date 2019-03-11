70dc88df9b41419a0f93e030e763bec59f87a737
optical3mild <chupa.pal.chad@gmail.com>
2019-03-12 05:54:54 +0900

    Introduce ver01

--- js/added_func.js
+++ js/added_func.js
@@ -1,15 +1,3 @@
-// $('#navbar li').click(function (e) {
-//   e.preventDefault()
-//   $(this).tab('show')
-// })
-
-
 $('.carousel').carousel({
   interval: false
 })
-
-function resizeCarousel() {
-  var sizeCarousel = document.getElementById("myCarousel");
-  var sizeOfWindow = screen.avaliHeight;
-  #myCarousel.style.height = sizeOfwindow;
-}  //????????
---
+++ self-introduce/js/added_func.js
@@ -0,0 +1,9 @@
+// $('#navbar li').click(function (e) {
+//   e.preventDefault()
+//   $(this).tab('show')
+// })
+
+
+$('.carousel').carousel({
+  interval: false
+})
